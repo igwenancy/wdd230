ea64d82ed5a5d15efe43340b79ebc613b76d308c
igwenancy <nancy.igwe@outlook.com>
2023-01-31 02:16:26 -0700

    home pGE

---
+++ chamber/scripts/index.js
@@ -0,0 +1,18 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('.menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+
+// last modified
+
+document.getElementById('updatedDate').innerHTML = document.lastModified;
+
+document.getElementById('copyYear').innerHTML = new Date().getFullYear(); // copyright year
+
+const images = document.querySelectorAll("[data-src]");
+
