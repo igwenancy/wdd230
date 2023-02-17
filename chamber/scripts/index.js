4edbdd67cef9023868bfc6c2f95c03fdd202c39c
igwenancy <nancy.igwe@outlook.com>
2023-02-16 23:21:42 -0700

    discover

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -16,3 +16,27 @@ document.getElementById('copyYear').innerHTML = new Date().getFullYear(); // cop
 
 const images = document.querySelectorAll("[data-src]");
 
+
+// Display the amount of time in days
+//  Using local storage.
+
+// Get the first day.
+const day1 = new Date();
+const firstDay = day1.getDay();
+
+// Set firstday in local storage
+localStorage.setItem('firstDay', firstDay);
+console.log(localStorage)
+console.log(localStorage.getItem('firstDay'));
+
+// Get the current day of the user visit
+const userDay = new Date();
+let userDays = userDay.getDay();
+
+// Find the difference between the days.
+let fd = localStorage.getItem('firstDay');
+console.log(userDays - parseInt(fd))
+
+// Get the element in html and set its textContent to days elapsed.
+let daysElapsed = document.querySelector('.last-visited');
+daysElapsed.textContent = userDays - parseInt(fd);
