26c85a3b463557b97dcdc795b527cf1ff9d6af22
igwenancy <nancy.igwe@outlook.com>
2023-02-17 20:30:35 -0700

    weather fixed

--- week05/scripts/weather.js
+++ week05/scripts/weather.js
@@ -1,32 +1,22 @@
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=Trier,us&units=imperial&appid=8b8247dc4bef2750033c5fe6b9eb9b91`;
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
-    
-    async function apiFetch() {
-        try {
-          const response = await fetch(url);
-          if (response.ok) {
-            const data = await response.json();
-            console.log(data); // testing only
-            // displayData(data); // uncomment when ready
-          } else {
-              throw Error(await response.text());
-          }
-        } catch (error) {
-            console.log(error);
-        }
-      }
-      
-      apiFetch();
-      
 
-function displayResults(data) {
-    currentTemp.innerHTML = `${data._____}&deg;F`;
-    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-    let desc = data.weather[0].description;
-    weatherIcon.setAttribute('src', iconsrc);
-    weatherIcon.setAttribute('alt', desc);
-    captionDesc.textContent = `${desc}`;
-  }
+
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Trier,us&units=imperial&appid=8b8247dc4bef2750033c5fe6b9eb9b91"
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+        weather(jsObject);
+    });
+
+function weather(jsObject){
+    document.querySelector('#current-temp').textContent = jsObject.main.temp;
+    const iconsrc = `http://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+    document.querySelector('#desc').textContent = jsObject.weather[0].description;
+    document.querySelector('#wind-speed').textContent = jsObject.wind.speed;
+}
+
